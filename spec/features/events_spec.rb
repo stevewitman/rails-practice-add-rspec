6e910a69b050d025f4d66576c7b880e0b4f78262
Steve Witman <stevewitman@gmail.com>
2014-11-04 12:58:44 -0700

    added first test

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
-feature "Events" do
+feature "User creates an event" do
 
-  scenario "Creating an event" do
-    visit '/events/new'
+  scenario "user sees the new event page" do
+    visit '/events'
     expect(page).to have_no_content("A test event")
+    click_on "New Event"
     fill_in "Description", with: "A test event"
+    fill_in "Location", with: "A test location"
+    fill_in "Capacity", with: 3
+    click_on "Create Event"
+    expect(page).to have_content("A test event")
+    expect(page).to have_content("A test location")
+
     save_and_open_page
   end
 
